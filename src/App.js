22ef4669bbf4d3dc02f33f4f75b786f7e40c2d91
unknown <fabioxsalvi@gmail.com>
2018-04-19 17:55:06 +0200

    Set up react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
-const App = () => <div>Welcome!</div>
+const App = () => 
+  <div>
+    <Route exact path="/" component={HomePage} />
+    <Route exact path="/login" component={LoginPage} />
+  </div>;
 
 export default App;
