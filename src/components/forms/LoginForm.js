2e09ca49267229aa90b77bb5a50b14725ebb5811
unknown <fabioxsalvi@gmail.com>
2018-04-19 20:26:32 +0200

    Added login form fields and input validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
 
 class LoginForm extends React.Component {
 
-    state = {}
+    state = {
+        data: {
+            email: '',
+            password: ''
+        },
+        loading: false,
+        errors: {}
+    }
+
+    // this is in abstract form in order to avoid using two functions: onChangeEmail, onChangePassword 
+    onChange = e => this.setState({ 
+        data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+    }
+
+    // TO DO: show errors if any
+    validate = (data) => {
+        const errors = {};
+        if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+        if ((!data.password) || (data.password.length < 8))
+            errors.password = "Password must be at least 8 characters long";
+        return errors;
+    }
 
     render() {
+        const { data } = this.state;
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="youremail@here.com"
+                        value={data.email}
+                        onChange={this.onChange}
+                    />   
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="your password here"
+                        value={data.password}
+                        onChange={this.onChange}
+                    />
+                </Form.Field>
                 <Button primary>Login</Button>
             </Form>
         )
