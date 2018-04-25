9c88e7fb92b80ea3a76675977ce0df6c66bd93b2
unknown <fabioxsalvi@gmail.com>
2018-04-25 21:12:08 +0200

    Styled StatePanel

--- src/components/panels/StatePanel.js
+++ src/components/panels/StatePanel.js
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Message } from "semantic-ui-react";
 import { connect } from "react-redux";
 
 // this is used to show an header with Redux' current state name.
 const StatePanel = ({ currentState }) => (
   <div>
-    <h1 style={{ textAlign: "center" }}>Current state is: {currentState}</h1>
+    <Message
+      header="Current state:"
+      content={currentState}
+      style={{
+        marginBottom: "2em",
+        marginTop: "2em"
+      }}
+    />
   </div>
 );
 
